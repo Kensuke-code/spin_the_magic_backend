a2ec86b9124a96b23ae0f2b4c45063bd452049e0
Kensuke-code <kenzo71719083@gmail.com>
2023-11-04 00:21:53 +0900

    不要なコード削除、コメント追加

--- app/services/fetch_wait_time_service.rb
+++ app/services/fetch_wait_time_service.rb
@@ -3,9 +3,7 @@ require 'uri'
 require 'json'
 
 class FetchWaitTimeService
-  def initialize
-    @wait_time_info = []
-  end
+  def initialize ; end
 
   def execute
     cached_wait_times
@@ -15,11 +13,16 @@ class FetchWaitTimeService
   private
 
     def cached_wait_times
+
+      # スクレイピングを走らせるため、提供元のサイトへの負荷を軽減させるため
       Rails.cache.fetch("wait_time", expires_in: 1.day) do
         park = 'sea'
         uri = URI("https://r5yd59ix1d.execute-api.ap-northeast-1.amazonaws.com/v1/waittime\?park\=#{park}")
         response = Net::HTTP.get(uri)
+
         result = JSON.parse(response)
+        # resultの例
+        # [{ "name": "インディージョーンズ", "condition": "案内終了" },{ "name": "レイジングスピリッツ", "condition": "案内終了" }]
     
         # 失敗時だと{"message"=>"Missing Authentication Token"}と配列サイズが1で返ってくるため
         result.length > 1 ? result : []
